d562baf76c016a382d63e6bc2f609c5db4930af0
vladbxb <vladbxb@gmail.com>
2025-09-05 08:21:31 +0300

    Renamed some variables

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def ensure_cookie() -> Response | None:
     if problem:
         return None
     code = fetch_code(default_problem)
-    resp = make_response(render_template('template.html', code=code, not_answered="true"))
+    resp = make_response(render_template('template.html', code=code, compile_answered=True))
     resp.set_cookie('problem', f'{default_problem}')
     return resp
 
@@ -43,7 +43,7 @@ def handle_compile(compiles, problem, code, return_code, stdout):
     if not compiles:
         if return_code != 0:
             if request.method == 'GET':
-                return render_template('template.html', code=code, not_comp="true", textarea=code, not_answered=None, correct_choice="true", stdout=stdout)
+                return render_template('template.html', code=code, not_comp=True, textarea=code, compile_answered=None, correct_choice=True, stdout=stdout)
             
             source = request.form['source_code']
             result = None
@@ -56,26 +56,26 @@ def handle_compile(compiles, problem, code, return_code, stdout):
 
                 result = subprocess.run(['g++', source_path, '-o', output_path], text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=compiler_timeout)
             if result.returncode != 0:
-                return render_template('template.html', code=code, not_comp="true", textarea=source, not_answered=None, correct_choice=None, stdout=stdout, answered="true", correct_answer=None, answer_stdout=result.stdout)
+                return render_template('template.html', code=code, not_comp=True, textarea=source, compile_answered=None, correct_choice=None, stdout=stdout, textarea_answered=True, correct_answer=None, answer_stdout=result.stdout)
             else:
-                return render_template('template.html', code=code, not_comp="true", textarea=source, not_answered=None, correct_choice=None, stdout=stdout, answered="true", correct_answer="true", answer_stdout=result.stdout)
+                return render_template('template.html', code=code, not_comp=True, textarea=source, compile_answered=None, correct_choice=None, stdout=stdout, textarea_answered=True, correct_answer=True, answer_stdout=result.stdout)
         else:
-            return render_template('template.html', code=code, comp="true", not_answered=None, correct_choice=None, stdout=stdout)
+            return render_template('template.html', code=code, comp=True, compile_answered=None, correct_choice=None, stdout=stdout)
     else:
         # if the user chose compile at the start
         if return_code == 0:
             if request.method == 'GET':
-                return render_template('template.html', code=code, comp="true", correct_choice="true")
+                return render_template('template.html', code=code, comp=True, correct_choice=True)
             output = request.form['output_answer']
 
             result = run(problem)
 
             if output.strip() == result.strip():
-                return render_template('template.html', code=code, comp="true", correct_choice="true", correct_answer="true", answered="true", answer_stdout=result, answered_value=output)
+                return render_template('template.html', code=code, comp=True, correct_choice=True, correct_answer=True, textarea_answered=True, answer_stdout=result, answered_value=output)
             else:
-                return render_template('template.html', code=code, comp="true", correct_choice="true", correct_answer=None, answered="true", answer_stdout=result, answered_value=output)
+                return render_template('template.html', code=code, comp=True, correct_choice=True, correct_answer=None, textarea_answered=True, answer_stdout=result, answered_value=output)
         else:
-            return render_template('template.html', code=code, not_comp="true", textarea=code, stdout=stdout)
+            return render_template('template.html', code=code, not_comp=True, textarea=code, stdout=stdout)
 
 @app.route('/')
 def index():
@@ -84,12 +84,9 @@ def index():
         return resp
     problem = request.cookies.get('problem')
     code = fetch_code(int(problem))
-    # not_answered is for the initial compile question
-    # 
-    #
-    #
-    # and answered is for the second compile question (pretty bad design ig)
-    return make_response(render_template('template.html', code=code, not_answered="true"))
+    # compile_answered is for the initial compile question
+    # and textarea_answered is for the second compile question (pretty bad design ig)
+    return make_response(render_template('template.html', code=code, compile_answered=True))
 
 @app.route('/compiles', methods=['GET', 'POST'])
 def compiles():
